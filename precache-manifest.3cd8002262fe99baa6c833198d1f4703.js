4c53b66e2a044a91e0cdfeb28fa85957d8b448af
michPhil <michelle.phillip@digi-x.my>
2019-05-15 23:14:19 +0800

    Updates

---
+++ precache-manifest.3cd8002262fe99baa6c833198d1f4703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f8431e7f25e7068564cd24ec9afda4",
+    "url": "/react-task-1/index.html"
+  },
+  {
+    "revision": "c33cd5fd391686aceb3d",
+    "url": "/react-task-1/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "03a71f10aaf2a5080fdd",
+    "url": "/react-task-1/static/js/2.cdea6d2f.chunk.js"
+  },
+  {
+    "revision": "c33cd5fd391686aceb3d",
+    "url": "/react-task-1/static/js/main.9dc0d6e3.chunk.js"
+  },
+  {
+    "revision": "2a1e2289a259436f7485",
+    "url": "/react-task-1/static/js/runtime~main.a7427b77.js"
+  }
+]);
